3ef0c5a9f44ff424c51301c2faacd895c8f588af
alialitwo037-gif <alialitwo037@gmail.com>
2026-09-19 21:55:55 +0300

    Update MainActivity.kt

--- app/src/main/java/com/wwm/arabictranslator/MainActivity.kt
+++ app/src/main/java/com/wwm/arabictranslator/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.wwm.arabictranslator
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        val textView = TextView(this).apply {
-            text = "WWM Arabic Translator - Base Ready"
-            textSize = 20f
-            setPadding(50, 50, 50, 50)
-        }
-        setContentView(textView)
+        setContentView(R.layout.activity_main)
     }
 }
